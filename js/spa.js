8cac0fcc78e46d8eaf14301387bf9007bd559f34
liuestc <1063506577@qq.com>
2017-02-06 15:10:21 +0800

    spa 单页应用 chat 模块

---
+++ js/spa.js
@@ -0,0 +1,15 @@
+var spa=(function(){
+	var initModule=function($container){
+		spa.shell.initModule($container)
+	}
+	return {initModule:initModule}
+})()
+
+
+
+// var spa=(function(){
+// 	var initModule=function($container){
+// 		$container.html("hello world")
+// 	}
+// 	return {initModule:initModule}
+// })()
